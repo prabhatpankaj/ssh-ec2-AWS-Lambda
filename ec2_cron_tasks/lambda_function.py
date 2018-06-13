ce7380ed03e48c08fce609bbc043f88f6e48d46c
prabhat pankaj <prabhatiitbhu@gmail.com>
2018-06-13 07:04:32 +0530

    Update lambda_function.py

--- ec2_cron_tasks/lambda_function.py
+++ ec2_cron_tasks/lambda_function.py
@@ -1,1 +1,27 @@
 
+import boto3
+
+
+def trigger_handler(event, context):
+    #Get IP addresses of EC2 instances
+    client = boto3.client('ec2')
+    instDict=client.describe_instances(
+            Filters=[{'Name':'tag:Name','Values':['myec2cronjob']}]
+        )
+    hostList=[]
+    for r in instDict['Reservations']:
+        for inst in r['Instances']:
+            hostList.append(inst['PublicIpAddress'])
+    #Invoke worker function for each IP address
+    client = boto3.client('lambda')
+    for host in hostList:
+        print "Invoking ec2_cron_function on " + host
+        invokeResponse=client.invoke(
+            FunctionName='ec2_cron_function',
+            InvocationType='Event',
+            LogType='Tail',
+            Payload='{"IP":"'+ host +'"}'
+        )
+    return{
+        'message' : "ec2 cron task started"
+    }
